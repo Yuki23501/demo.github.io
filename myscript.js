780b5e8f4684a5681a1bfcc3c1ad65293a1f2c2b
Yuki23501 <82694568+Yuki23501@users.noreply.github.com>
2022-07-02 19:15:54 +0900

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -1,34 +1,26 @@
-function play_se(){
-	var warning = new Audio('warning.mp3');
-	var voice = new Audio('voice.mp3');
-	warning.play();
-	voice.play();
-}
+ $(function(){
+        // ブラウザバック禁止
+        history.pushState(null, null, null);
+        $(window).on("popstate", function(e){
+          if (!e.originalEvent.state){
+            play_se();
+            history.pushState(null, null, null);
+            return;
+          }
+        });
 
+        // モーダル初期化
+        $('.modal').modal({dismissible: false});
+        $('#alert').modal('open');
+        $('#close').click(function(){
+          $('#alert').modal('close');
+          play_se();
+        });
 
-   history.pushState(null, null, null)
-   $(window).on('popstate', function(e){if(!e.orininalEvent.state){
-play_se();
-history.pushState(null,null,null);
-return;
-}
-});
-
-   $('.modal').modal({dismissible: false});
-   $('#alert').modal('open');
-   $('#close').click(function(){
-       $('#alert').modal('close');
-       play_se()
-   });
-   
-
-
-    var device = navigator.userAgent.match(/Android|iPhone|iPad/);
-    if (device == null){
-    	device = '端末';
-    }
-
-
+        var device = navigator.userAgent.match(/Android|iPhone|iPad/);
+        if (device == null){
+          device = '端末';
+        }
     $('#device').text(device);
 
     var time = 200;
